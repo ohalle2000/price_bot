d36e1596984cd06d275ccd3f520abd96fe4e5251
goftok <goftok@goftoks-MacBook-Pro.local>
2024-03-13 02:06:42 +0100

    changing to fetch api

--- _utils.py
+++ _utils.py
@@ -8,24 +8,26 @@ console = Console()
 def get_website_config(name: str):
     with open("./config.json", "r") as file:
         config = json.load(file)
-    return config["websites"][name]
+    return config[name]
 
 
 def send_telegram_message(bot_token: str, admin_id: int, message: str):
     base_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    payload = {"chat_id": admin_id, "text": message}
+    payload = {"chat_id": admin_id, "text": message, "parse_mode": "HTML"}
     response = requests.post(base_url, data=payload)
     return response.json()
 
 
-def expand_link_with_pages(link: str, test_to_expand: str, number_of_pages: int) -> list:
-    links = []
-    links.append(link + test_to_expand)
-    for i in range(1, number_of_pages + 1):
-        result_link = link + f"p/{i}/" + test_to_expand
-        links.append(result_link)
-    return links
+def open_file(file_path: str):
+    """Read lines from a file or create it if not exists."""
+    try:
+        with open(file_path, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return []
 
 
-def custom_sort(link):
-    return ("https" in link, link)
+def write_file(file_path: str, data1: list, data2: list):
+    """Write list of data to a file, each on a new line."""
+    with open(file_path, "w") as file:
+        file.write("\n".join(data1 + data2))
--- main.py
+++ main.py
@@ -1,52 +1,95 @@
+import requests
 import time
-from _utils import *
+import markdown2
+from urllib.parse import urlencode, quote
+from _utils import open_file, write_file, get_website_config, send_telegram_message
+from _secrets import BOT_TOKEN, ADMIN_ID
+from deep_translator import GoogleTranslator
+from rich.console import Console
 
-# from _macbook import macbook_main
-from _cars import cars_main
+# Constants
+SLEEP_TIME = 60
+LIMIT = 100
+CAR_FILE = "./cars2.txt"
+CONFIG_SOURCE = "cars-2dehands"
 
-from selenium import webdriver
-
-SLEEP_TIME = 5 * 60
-
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
-)
-browser = webdriver.Chrome(options=options)
+console = Console()
+translator = GoogleTranslator(source="auto", target="en")
 
 
-# use without cookies
+def translate_to_english(text):
+    """Translate text to English using Google Translator."""
+    return translator.translate(text)
+
+
+def escape_markdown(text):
+    """Escape special characters for MarkdownV2"""
+    escape_chars = "_*[]()~`>#+-=|{}.!"
+    return "".join(f"\\{char}" if char in escape_chars else char for char in text)
+
+
+def create_bot_message(car: dict, config: dict):
+    price_euro = car["priceInfo"]["priceCents"] / 100
+    price_type = car["priceInfo"]["priceType"]
+    listing_url = f"{config['main_link']}{car['vipUrl']}"
+
+    car_attributes = {attr["key"]: attr["value"] for attr in car["attributes"]}
+
+    model = car["vipUrl"].split("/")[3]
+
+    message = f"🚗 **New Car Listing Found!**\n"
+    message += f"#{model}\n"
+    message += f"🚘 Title: {translate_to_english(car['title'])}\n"
+    message += f"💰 Price: €{price_euro} ({price_type})\n"
+    message += f"📍 Location: {car['location']['cityName']}, {car['location']['countryName']}\n"
+    message += f"🗒️ Description: {translate_to_english(car['categorySpecificDescription'])}\n"
+    message += f"📅 Year: {car_attributes.get('constructionYear')}\n"
+    message += f"🛣️ Km: {car_attributes.get('mileage', 'N/A')} km\n"
+    message += f"⛽ Fuel: {car_attributes.get('fuel', 'N/A')}\n"
+    message += f'🔗 <a href="{listing_url}">View Listing</a>\n'
+    return message
+
+
+def create_urls(url_number: int = 5, config: dict = None, limit: int = 100):
+    if config is None:
+        config = {}
+
+    urls = []
+    for i in range(url_number):
+        query_params = config.copy()
+        query_params["offset"] = i * limit
+        query_params["limit"] = limit
+
+        url = f"{config.get('api_link', '')}?{urlencode(query_params)}"
+        urls.append(url)
+
+    return urls
+
+
 def main():
+    config = get_website_config(CONFIG_SOURCE)
+    urls = create_urls(url_number=5, config=config, limit=LIMIT)
+
     while True:
-        for search in ["car-marketplaats", "car-2dehands"]:
-            config = get_website_config(search)
-            base_url = config["url"]
-            target = config["target"]
-            parameters = config["parameters"]
-            max_price = config["max_price"]
+        try:
+            cars = (item for url in urls for item in requests.get(url).json()["listings"])
+            filtered_cars = [car for car in cars if car["priceInfo"]["priceCents"] <= config["max_price"]]
 
-            urls = expand_link_with_pages(base_url, parameters, 5)
+            sorted_cars = sorted(filtered_cars, key=lambda x: x["itemId"], reverse=True)[:100]
+            checked_cars = open_file(CAR_FILE)
 
-            with open("./car-links.txt", "r") as file:
-                links = file.read().splitlines()
+            new_cars = [car for car in sorted_cars if car["itemId"] not in checked_cars]
+            for car in new_cars:
+                bot_message = create_bot_message(car, config)
+                print(bot_message)
+                send_telegram_message(BOT_TOKEN, ADMIN_ID, bot_message)
 
-            macbook_links = cars_main(browser, urls, base_url, target, max_price, links)
+            write_file(CAR_FILE, [car["itemId"] for car in new_cars], checked_cars)
 
-            # Sort links alphabetically
-            macbook_links.sort(key=custom_sort)
+        except requests.RequestException as e:
+            console.log(f"Error fetching data: {e}")
 
-            # Limit to the last 50 links
-            # existing_links = existing_links[-50:]
-
-            with open("./car-links.txt", "w") as file:
-                file.write("\n".join(macbook_links))
-
-            console.print("[bold blue]Sleeping...[/bold blue]")
         time.sleep(SLEEP_TIME)
-
-    browser.quit()
 
 
 if __name__ == "__main__":
