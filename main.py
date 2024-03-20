e456af8a575f57e7ad91c0ed3515ebe6205dd100
goftok <goftok@goftoks-MacBook-Pro.local>
2024-03-20 20:46:37 +0100

    filter_ads  refactor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 from _cars import create_cars_bot_message
 from _wheels import create_wheels_bot_message
-from _utils import get_website_config, create_urls,send_telegram_message, console
+from _utils import get_website_config, create_urls, send_telegram_message, console
 from _secrets import BOT_TOKEN, CHAT_ID1, CHAT_ID2, CHAT_ID3
 
 # Constants
@@ -19,49 +19,32 @@ CONFIG_SOURCE4 = "wheels-2dehands"
 
 CONFIG_PRICE3_4 = 50000
 
-allowed_models =['audi', 'volkswagen', 'seat', 'skoda', 'bmw', 'mini', 'lexus']
+allowed_models = ["audi", "volkswagen", "seat", "skoda", "bmw", "mini", "lexus"]
 CONFIG_PRICE5 = 1000000
 
-def filter_ads1(ads: list, max_price: int, newest_car_id: str = None) -> list:
 
-    filtered_ads = [
-        ad for ad in ads
-        if ad["priceInfo"]["priceCents"] <= max_price and (newest_car_id is None or ad["itemId"] > newest_car_id)
-    ]
-    sorted_cars = sorted(filtered_ads, key=lambda x: int(x["itemId"][1:]), reverse=True)
-
-    if not sorted_cars:
-        return newest_car_id, list()
-    
-    # dont send cars from first iteration
-    if not newest_car_id:
-        return sorted_cars[0]["itemId"], list()
-    
-    newest_car_id = sorted_cars[0]["itemId"]
-    console.print(f"Newest id: {newest_car_id}")
-    return newest_car_id, sorted_cars
-
-def filter_ads2(ads: list, max_price: int, newest_car_id: str = None) -> list:
-
+def filter_ads(ads: list, max_price: int, newest_car_id: str = None, allowed_models: list = None) -> tuple:
     filtered_ads = []
+
     for ad in ads:
         if ad["priceInfo"]["priceCents"] <= max_price and (newest_car_id is None or ad["itemId"] > newest_car_id):
-            model= ad["vipUrl"].split("/")[3]
-            if model in allowed_models:
+            model = ad["vipUrl"].split("/")[3] if allowed_models is not None else None
+
+            if allowed_models is None or model in allowed_models:
                 filtered_ads.append(ad)
 
-    sorted_cars = sorted(filtered_ads, key=lambda x: int(x["itemId"][1:]), reverse=True)
+    sorted_ads = sorted(filtered_ads, key=lambda x: int(x["itemId"][1:]), reverse=True)
 
-    if not sorted_cars:
-        return newest_car_id, list()
-    
-    # dont send cars from first iteration
-    if not newest_car_id:
-        return sorted_cars[0]["itemId"], list()
-        
-    newest_car_id = sorted_cars[0]["itemId"]
+    if not sorted_ads:
+        return newest_car_id, []
+
+    newest_car_id = sorted_ads[0]["itemId"]
     console.print(f"Newest id: {newest_car_id}")
-    return newest_car_id, sorted_cars
+
+    if newest_car_id is None:
+        return sorted_ads[0]["itemId"], []
+
+    return newest_car_id, sorted_ads
 
 
 def check_ads(config: dict, filtered_ads: list, chat_id: str, function_for_message):
@@ -73,7 +56,8 @@ def check_ads(config: dict, filtered_ads: list, chat_id: str, function_for_messa
     except Exception as e:
         console.log(f"Error fetching data: {e}")
         send_telegram_message(BOT_TOKEN, chat_id, f"Error fetching data. Check logs for more info. {e}")
-        
+
+
 def main():
     config1 = get_website_config(CONFIG_SOURCE1)
     config2 = get_website_config(CONFIG_SOURCE2)
@@ -95,62 +79,62 @@ def main():
     while True:
         cars1 = [item for url in urls1 for item in requests.get(url).json()["listings"]]
         cars3 = cars1.copy()
-        newest_car_id_1, cars1 = filter_ads1(cars1, CONFIG_PRICE1_2, newest_car_id_1)
+        newest_car_id_1, cars1 = filter_ads(cars1, CONFIG_PRICE1_2, newest_car_id_1)
 
         check_ads(
-            config=config1, 
-            filtered_ads = cars1,
+            config=config1,
+            filtered_ads=cars1,
             chat_id=CHAT_ID1,
-            function_for_message=create_cars_bot_message, 
+            function_for_message=create_cars_bot_message,
         )
 
         cars2 = [item for url in urls2 for item in requests.get(url).json()["listings"]]
         cars4 = cars2.copy()
-        newest_car_id_2, cars2 = filter_ads1(cars2, CONFIG_PRICE1_2, newest_car_id_2)
+        newest_car_id_2, cars2 = filter_ads(cars2, CONFIG_PRICE1_2, newest_car_id_2, allowed_models)
 
         check_ads(
             config=config2,
-            filtered_ads=cars2, 
+            filtered_ads=cars2,
             chat_id=CHAT_ID1,
-            function_for_message=create_cars_bot_message, 
+            function_for_message=create_cars_bot_message,
         )
 
-        newest_car_id_3, cars3 = filter_ads2(cars3, CONFIG_PRICE5, newest_car_id_3)
+        newest_car_id_3, cars3 = filter_ads(cars3, CONFIG_PRICE5, newest_car_id_3)
 
         check_ads(
-            config=config1, 
-            filtered_ads=cars3, 
+            config=config1,
+            filtered_ads=cars3,
             chat_id=CHAT_ID2,
-            function_for_message=create_cars_bot_message, 
+            function_for_message=create_cars_bot_message,
         )
 
-        newest_car_id_4, cars4 = filter_ads2(cars4, CONFIG_PRICE5, newest_car_id_4)
+        newest_car_id_4, cars4 = filter_ads(cars4, CONFIG_PRICE5, newest_car_id_4, allowed_models)
 
         check_ads(
             config=config2,
-            filtered_ads=cars4, 
+            filtered_ads=cars4,
             chat_id=CHAT_ID2,
-            function_for_message=create_cars_bot_message, 
+            function_for_message=create_cars_bot_message,
         )
 
         wheels1 = [item for url in urls3 for item in requests.get(url).json()["listings"]]
-        newest_wheel_id_1, wheels1 = filter_ads1(wheels1, CONFIG_PRICE3_4, newest_wheel_id_1)
+        newest_wheel_id_1, wheels1 = filter_ads(wheels1, CONFIG_PRICE3_4, newest_wheel_id_1)
 
         check_ads(
-            config=config3, 
-            filtered_ads=wheels1, 
+            config=config3,
+            filtered_ads=wheels1,
             chat_id=CHAT_ID3,
-            function_for_message=create_wheels_bot_message, 
+            function_for_message=create_wheels_bot_message,
         )
 
         wheels2 = [item for url in urls4 for item in requests.get(url).json()["listings"]]
-        newest_wheel_id_2, wheels2 = filter_ads1(wheels2, CONFIG_PRICE3_4, newest_wheel_id_2)
-        
+        newest_wheel_id_2, wheels2 = filter_ads(wheels2, CONFIG_PRICE3_4, newest_wheel_id_2)
+
         check_ads(
-            config=config4, 
-            filtered_ads=wheels2, 
+            config=config4,
+            filtered_ads=wheels2,
             chat_id=CHAT_ID3,
-            function_for_message=create_wheels_bot_message, 
+            function_for_message=create_wheels_bot_message,
         )
 
         print(newest_car_id_1, newest_car_id_2, newest_car_id_3, newest_car_id_4, newest_wheel_id_1, newest_wheel_id_2)
---
+++ toadd.py
@@ -0,0 +1,42 @@
+# main_config = {
+#     "cars_2dehands": {
+#         "source": "cars-2dehands",
+#         "max_price": 500000,
+#         "chat_id": CHAT_ID1,
+#         "allowed_models": None,
+#         "url_number": 30,
+#     },
+#     "cars_marktplaats": {
+#         "source": "cars-marktplaats",
+#         "max_price": 500000,
+#         "chat_id": CHAT_ID1,
+#         "allowed_models": None,
+#         "url_number": 30,
+#     },
+#     "cars_2dehands": {
+#         "source": "cars-2dehands",
+#         "max_price": 500000,
+#         "chat_id": CHAT_ID2,
+#         "allowed_models": ["audi", "volkswagen", "seat", "skoda", "bmw", "mini", "lexus"],
+#         "url_number": 30,
+#     },
+#     "cars_marktplaats": {
+#         "source": "cars-marktplaats",
+#         "max_price": 500000,
+#         "chat_id": CHAT_ID2,
+#         "allowed_models": ["audi", "volkswagen", "seat", "skoda", "bmw", "mini", "lexus"],
+#         "url_number": 30,
+#     },
+#     "wheels_2dehands": {
+#         "source": "wheels-2dehands",
+#         "max_price": 50000,
+#         "chat_id": CHAT_ID3,
+#         "url_number": 2,
+#     },
+#     "wheels_marktplaats": {
+#         "source": "wheels-marktplaats",
+#         "max_price": 50000,
+#         "chat_id": CHAT_ID3,
+#         "url_number": 2,
+#     },
+# }
